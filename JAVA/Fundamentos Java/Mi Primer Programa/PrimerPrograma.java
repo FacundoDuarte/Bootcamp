327aea8c9d66294d3deb96cf626108c45b48420b
Facundo Duarte <facundoduarte1313@gmail.com>
2023-07-11 21:48:35 -0300

    Se agrega la carpeta JAVA con el ejercicio 'Primer Programa'

---
+++ JAVA/Fundamentos Java/Mi Primer Programa/PrimerPrograma.java
@@ -0,0 +1,7 @@
+public class PrimerPrograma{
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Facundo Duarte");
+System.out.println("Tengo 20 años de edad.");
+System.out.println("Mi ciudad es Montevideo, Uruguay");
+    }
+}
